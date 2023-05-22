ceedb668f45b153b55f4fd3101ecc972d13f90fa
SusaMarko <markosusa567@gmail.com>
2023-05-22 17:22:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  res.send("hello");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
