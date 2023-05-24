2412e68be3b7106dc559795f52f45538db2e0d70
Marko Susa <markosusa567@gmail.com>
2023-05-24 21:48:03 +0200

    modify fishing_report table

--- database.sql
+++ database.sql
@@ -1,15 +1,13 @@
 CREATE DATABASE fishtrack;
 
 CREATE TABLE fishing_report(
-    fishing_report_id SERIAL PRIMARY KEY,
-    author VARCHAR(255),
-    date VARCHAR(255),
-    time VARCHAR(255),
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP,
     spot VARCHAR(255),
-    water_level VARCHAR(255),
+    water_level INTEGER,
     weather VARCHAR(255),
     type_of_fishing VARCHAR(255),
     bait VARCHAR(255),
     food VARCHAR(255),
-    catch VARCHAR(255)
+    the_catch VARCHAR(255)
 );
