fbd3ba076b34bb58c1e8bfa32c6a121f0f51e2e4
Marko Susa <markosusa567@gmail.com>
2023-05-29 18:14:50 +0200

    add cors and remove /hello endpoint

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const pool = require("./db");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-
-app.get("/hello", (req, res) => {
-  res.send("hello");
-});
+app.use(cors());
 
 app.get("/api/v1/fishing-reports", async (req, res) => {
   const fishingReport = await pool.query("SELECT * FROM fishing_report");
