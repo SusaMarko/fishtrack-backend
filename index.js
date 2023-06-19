c58e7bf683bdb842c0abfbfe9bf032eb7dfe949e
Marko Susa <markosusa567@gmail.com>
2023-06-19 19:34:22 +0200

    add fishing report creation route

--- index.js
+++ index.js
@@ -13,6 +13,37 @@ app.get("/api/v1/fishing-reports", async (req, res) => {
   res.json(fishingReport.rows);
 });
 
+app.post("/api/v1/fishing-reports", async (req, res) => {
+  try {
+    const {
+      createdAt,
+      spot,
+      waterLevel,
+      weather,
+      typeOfFishing,
+      bait,
+      food,
+      theCatch,
+    } = req.body;
+    const newFishingReport = await pool.query(
+      "INSERT INTO fishing_report (created_at, spot, water_level, weather, type_of_fishing, bait, food, the_catch) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *",
+      [
+        createdAt,
+        spot,
+        waterLevel,
+        weather,
+        typeOfFishing,
+        bait,
+        food,
+        theCatch,
+      ]
+    );
+    res.status(201).json(newFishingReport.rows[0]);
+  } catch (err) {
+    res.status(503);
+  }
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
